f41230dff0553371a718b852741c8c430efbb6f7
JulkipliAbdulla <113312693+Julkipli21@users.noreply.github.com>
2023-02-14 11:36:02 -0800

    Update sidebar.php

--- Admin-Side/inclusion/sidebar.php
+++ Admin-Side/inclusion/sidebar.php
@@ -1,139 +1,129 @@
-<?php
- require_once 'adhead.php';
- ?>
 
- <style>
-  .mdc-drawer .mdc-drawer__header .brand-logo {
-    display: block;
-    padding: 10px 0;
-  }
-   hr{
-    border: 1px solid gray;
-    width: 86%;
-   }
- </style>
+<div class="sidebar close">
+    <div class="logo-details">
+      <i class="fa-solid fa-circle-user"></i>
+      <span class="logo_name">Bernard Jay</span>
+    </div>
+    
+    <form action="../login/login.php">
+      <div class="logout-button">
+      <input type="submit" value="Logout" class="logout" >
+      </div>
+    </form>
+    <ul class="nav-links">
 
-<aside class="mdc-drawer mdc-drawer--dismissible mdc-drawer--open">
-      <div class="mdc-drawer__header">
-        <a href="#" class="brand-logo">
-          <img src="../image/azzuralogo.png" alt="logo" style="width: 170px; height: 50px;">
+    <hr>
+      <li>
+        <a href="../dashboard/dashboard.php" >
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name" href="#">Dashboard</span>
         </a>
-      </div>
-      <hr>
-      <div class="mdc-drawer__content">
-        <div class="mdc-list-group">
-          <nav class="mdc-list mdc-drawer-menu">
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="admin.html">
-              <i class='bx bx-grid-alt' ></i>
-                <span class="menu-bar">Dashboard</span>
-              </a>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="ui-sub-menu">
-              <i class="fa-solid fa-hotel"></i>
-                <span class="menu-bar">Accommodation</span>
-                <i class="mdc-drawer-arrow material-icons">chevron_right</i>
-              </a>
-              <div class="mdc-expansion-panel" id="ui-sub-menu">
-                <nav class="mdc-list mdc-drawer-submenu">
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                      Hotel Rooms
-                    </a>
-                  </div>
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                      Villas
-                    </a>
-                  </div>
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                      Cottages
-                    </a>
-                  </div>
-                </nav>
-              </div>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="ui-sub-menus">
-              <i class="fa-solid fa-tags"></i>
-                <span class="menu-bar">Discounts and Promos</span>
-                <i class="mdc-drawer-arrow material-icons">chevron_right</i>
-              </a>
-              <div class="mdc-expansion-panel" id="ui-sub-menus">
-                <nav class="mdc-list mdc-drawer-submenu">
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                      Discounts
-                    </a>
-                  </div>
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                      Promos
-                    </a>
-                  </div>
-                </nav>
-              </div>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class="fa-solid fa-coins"></i>
-                <span class="menu-bar">Payments</span>
-              </a>
-            </div>  
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class='bx bxs-report'></i>
-                <span class="menu-bar">Sales Report</span>
-              </a>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class="fa-solid fa-house-circle-check"></i>
-                <span class="menu-bar">Reservation</span>
-              </a>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-expansion-panel-link" href="#" data-toggle="expansionPanel" data-target="ui-sub-menus1">
-              <i class="fa-solid fa-users-gear"></i>
-                <span class="menu-bar">Accounts</span>
-                <i class="mdc-drawer-arrow material-icons">chevron_right</i>
-              </a>
-              <div class="mdc-expansion-panel" id="ui-sub-menus1">
-                <nav class="mdc-list mdc-drawer-submenu">
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                    <span class="menu-bar">Staff</span>
-                    </a>
-                  </div>
-                  <div class="mdc-list-item mdc-drawer-item">
-                    <a class="mdc-drawer-link" href="#">
-                    <span class="menu-bar">Customers</span>
-                    </a>
-                  </div>
-                </nav>
-              </div>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class='bx bxs-cube'></i>
-                <span class="menu-bar">Other Amenities</span>
-              </a>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class="fa-solid fa-folder-open"></i>
-                <span class="menu-bar">Archives</span>
-              </a>
-            </div>
-            <div class="mdc-list-item mdc-drawer-item">
-              <a class="mdc-drawer-link" href="#">
-              <i class='bx bx-cog' ></i>
-                <span class="menu-bar">Settings</span>
-              </a>
-            </div>
-          </nav>
+        <ul class="sub-menu blank">
+          <li>
+            <a class="link_name" href="#">Dashboard</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+       <div class="iocn-link">
+          <a href="#">
+          <i class="fa-solid fa-hotel"></i>
+            <span class="link_name">Accommodations</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
         </div>
-      </div>
-</aside>
+        <ul class="sub-menu">
+        
+          <li><a class="link_name" href="#">Accommodations</a></li>
+          <li><a href="../accommodations/hotel-rooms.php">Hotel Rooms</a></li>
+          <li><a href="../accommodations/villas.php">Villas</a></li>
+          <li><a href="../accommodations/cottages.php">Cottages</a></li>
+        </ul>
+        
+      </li>
+     
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+          <i class="fa-solid fa-tags"></i>
+            <span class="link_name">Discounts & Promos</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Discounts & Promos</a></li>
+          <li><a href="../discounts&promos/discounts.php">Discounts</a></li>
+          <li><a href="../discounts&promos/promos.php">Promos</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="../payments/payments.php">
+        <i class="fa-solid fa-coins"></i>
+          <span class="link_name">Payments</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" >Payments</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="../sales-report/sales-report.php">
+        <i class='bx bxs-report'></i>
+          <span class="link_name">Sales Report</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name">Sales Report</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="../reservation/reservation.php">
+        <i class="fa-solid fa-house-circle-check"></i>
+          <span class="link_name">Reservation</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" >Reservation</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+          <i class="fa-solid fa-users-gear"></i>
+            <span class="link_name">Accounts</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Accounts</a></li>
+          <li><a href="../accounts/admin.php">Admin</a></li>
+          <li><a href="../accounts/customers.php">Customers</a></li>  
+        </ul>
+      </li>
+      <li>
+        <a href="../other-amenities/other-amenities.php">
+        <i class="fa-solid fa-umbrella-beach"></i>
+          <span class="link_name">Other Amenities</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name">Other Amenities</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="../archives/archives.php">
+        <i class="fa-solid fa-folder-open"></i>
+          <span class="link_name">Archives</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name">Archives</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="../settings/settings.php">
+          <i class='bx bx-cog' ></i>
+          <span class="link_name">Settings</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name">Settings</a></li>
+        </ul>
+      </li>
+      
+</ul>
+  </div>
