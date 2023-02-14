1e39f4e7958ed2277f25b018c869d16ab699b4ba
JulkipliAbdulla <113312693+Julkipli21@users.noreply.github.com>
2023-02-14 11:33:32 -0800

    Update dashboard.php

--- Admin-Side/dashboard/dashboard.php
+++ Admin-Side/dashboard/dashboard.php
@@ -1,117 +1,140 @@
 <?php
-        //resume session here to fetch session values
-        session_start();
-        /*
-            if user is not login then redirect to login page,
-            this is to prevent users from accessing pages that requires
-            authentication such as the dashboard
-        */
-        if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
-            header('location: ../dashboard/dashboard.php');
-        }
 
+       //resume session here to fetch session values
+       session_start();
+       /*
+           if user is not login then redirect to login page,
+           this is to prevent users from accessing pages that requires
+           authentication such as the dashboard
+       */
+       if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] != 'admin'){
+           header('location: ../login/login.php');
+       }
+       //if the above code is false then html below will be displayed
+    //if the above code is false then html below will be displayed
+
+    require_once '../inclusion/sidebar.php';
     require_once '../tools/variables.php';
-    $page_title = 'Azzura | Dashboard';
-    $dashboard = 'active';
+    $dashboard="active";
 
-    require_once '../inclusion/header.php';
 ?>
-<style>
-    .mdc-card.info-card .card-inner .card-title{
-        font-size: 12px;
-        font-weight: 700;
-        margin-bottom: 9px;
-    }
-    .mdc-card.info-card .card-inner .card-icon-wrapper {
-    position: absolute;
-    right: 10px;
-    top: 60%;
-    transform: translateY(-50%);
-    width: 45px;
-    height: 45px;
-    border-radius: 2px;
-    background-color: #00b67a;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-align-items: center;
-    align-items: center;
-    -webkit-justify-content: center;
-    justify-content: center;
-    box-shadow: 0 0 10px 5px rgb(0 182 122 / 35%);
-}
-.mdc-card.info-card.info-card--success .card-inner .card-icon-wrapper {
-    background-color: #296d98;
-    box-shadow: 0 0 10px 5px rgb(0 182 122 / 35%);
-}
 
-</style>
-<div class="body-wrapper">
-    <?php require_once '../inclusion/adsidebar.php'; ?>
-        <div class="main-wrapper mdc-drawer-app-content">
-            <?php require_once '../inclusion/adheader.php'; ?>
-            <span class="mdc-top-app-bar__title"> <h3 style="margin-left: 37px;"> Dashboard </h3></span>
-                <div class="page-wrapper mdc-toolbar-fixed-adjust">
-                    <main class="content-wrapper">
-                    <div class="mdc-layout-grid">
-                        <div class="mdc-layout-grid__inner">
-                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
-                            <div class="mdc-card info-card info-card--success">
-                            <div class="card-inner">
-                                <h5 class="card-title">Available Hotel Rooms</h5>
-                                <h5 class="font-weight-light pb-2 mb-1 border-bottom">3</h5>
-                                <p class="tx-12 text-muted">out of 20</p>
-                                <div class="card-icon-wrapper">
-                                <i class="fa-solid fa-hotel"></i>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
-                            <div class="mdc-card info-card info-card--success">
-                            <div class="card-inner">
-                                <h5 class="card-title">Available Villas</h5>
-                                <h5 class="font-weight-light pb-2 mb-1 border-bottom">50</h5>
-                                <p class="tx-12 text-muted">out of 8</p>
-                                <div class="card-icon-wrapper">
-                                <i class="fa-solid fa-house-chimney"></i>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
-                            <div class="mdc-card info-card info-card--success">
-                            <div class="card-inner">
-                                <h5 class="card-title">Available Cottages</h5>
-                                <h5 class="font-weight-light pb-2 mb-1 border-bottom">100/200</h5>
-                                <p class="tx-12 text-muted">out of 50   </p>
-                                <div class="card-icon-wrapper">
-                                <i class="fa-solid fa-umbrella-beach"></i>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-3-desktop mdc-layout-grid__cell--span-4-tablet">
-                            <div class="mdc-card info-card info-card--success">
-                            <div class="card-inner">
-                                <h5 class="card-title">Monthly Sales</h5>
-                                <h5 class="font-weight-light pb-2 mb-1 border-bottom">&#8369 2000</h5>
-                                <p class="tx-12 text-muted">50% target reached</p>
-                                <div class="card-icon-wrapper">
-                                <i class="fa-solid fa-arrow-trend-up"></i>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
-                        </div>
-                        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/sidebar.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <script src="https://kit.fontawesome.com/0cc7e94eea.js" crossorigin="anonymous"></script>
+    <title>Dashboard</title>
+
+    
+<div class="home-section">
+    <div class="home-content">
+        <nav>
+            <div class="side-bar-button"><i class='bx bx-menu' ></i>
+            <span class="text" style="margin-bottom: 15px;">Dashboard</span> </div>
+        </nav>
+      <div class="overview-boxes">
+            <div class="box">
+                <div class="right-side">
+                    <div class="box-topic">Available Hotel Rooms</div>
+                    <div class="number">11</div>
+                    <div class="indicator">
+                        
                     </div>
-                    </main>
-                    <!-- partial:partials/_footer.html --> 
-                    <?php require_once '../inclusion/adfooter.php'; ?>
-                    <!-- partial -->
                 </div>
+                <i class="fa-sharp fa-solid fa-door-open" style="font-size:50px; margin-left: 60px;"></i>
+            </div>
+
+            <div class="box">
+                <div class="right-side">
+                    <div class="box-topic">Available Villa</div>
+                    <div class="number">23</div>
+                    <div class="indicator"></div>
+                </div>
+                <i class="fa-solid fa-house-chimney-window" style="font-size:50px; margin-left: 60px;"></i>
+            </div>
+
+            <div class="box">
+                <div class="right-side">
+                    <div class="box-topic">Availabe Cottages</div>
+                    <div class="number">21</div>
+                    <div class="indicator"></div>
+                </div>
+                <i class="fa-sharp fa-solid fa-house" style="font-size:50px; margin-left: 60px;"></i>
+            </div>
+
+            <div class="box">
+                <div class="right-side">
+                    <div class="box-topic">Monthly Sales</div>
+                    <div class="number">₱ 123, 456.00 </div>
+                    <div class="indicator"></div>
+                </div>
+                <i class="bx bxs-report" style="font-size:50px; margin-left: 60px;"></i>
+            </div>
+
+            <div class="box">
+                <div class="right-side">
+                    <div class="box-topic">Total Registered Users</div>
+                    <div class="number">123, 456</div>
+                    <div class="indicator"></div>
+                </div>
+                <i class="fa-sharp fa-solid fa-user" style="font-size:50px; margin-left: 60px;"></i>
+            </div>
+
+            <div class="box">
+                <div class="right-side">
+                <div>
+                    <p>Today is <?php echo date("l"); ?></p>
+                    <p>The date is <?php echo date("m/d/Y"); ?></p>
+                    <p id="time">The time is <?php echo date("h:i:sa"); ?></p>
+                </div>
+
+                <script>
+                    function updateTime() {
+                        var now = new Date();
+                        var time = document.getElementById("time");
+                        time.innerHTML = "The time is " + now.toLocaleTimeString();
+                    }
+
+                    setInterval(updateTime, 1000);
+                </script>
+
+
+                    </div>
+                    <img src="../image/event-date-and-time-symbol.png" style="margin-left: 50px;" width="50px" >
+                
+            </div>
         </div>
+    </div>
+
+
+    
 </div>
 
-<?php require_once '../inclusion/adend.php'; ?>
+
+
+
+  <script>
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });
+  </script>
+  
+</body>
+</html>
